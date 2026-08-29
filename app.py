bd78547d6be3dbe306a5108d9a0e2d1cd4a7ad6e
ofekmosko112 <spookytales0311@gmail.com>
2026-08-29 11:06:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ with st.sidebar:
         actual_model = "llama3"
         st.caption("🔒 פועל באופן מקומי מלא (Zero Data Leakage).")
     else:
-        client = OpenAI(api_key="DItyIfPjPQUAKeqEhUYX52hhpVtO9q7X3evcdzBqk65_XrJcTZKhtGWDfVt9Gyn8rCxz7CFSShT3BlbkFJKOtQuwiEf9QSuX9")
+        client = OpenAI(api_key=os.environ.get("DItyIfPjPQUAKeqEhUYX52hhpVtO9q7X3evcdzBqk65_XrJcTZKhtGWDfVt9Gyn8rCxz7CFSShT3BlbkFJKOtQuwiEf9QSuX9"))
         actual_model = "gpt-4o"
         st.caption("☁️ מחובר לשרתי הענן המאובטחים.")
 
